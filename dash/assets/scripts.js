48aa343997d941966fcc565e8efaf7dda4fad390
EmeraldMike <EmeraldMike360@gmail.com>
2022-03-03 14:14:03 -0500

    tests

--- dash/assets/scripts.js
+++ dash/assets/scripts.js
@@ -14,11 +14,20 @@ function que(){
 let quey = document.forms["form"]["quey"].value;
 try{
 quey = JSON.parse(quey)
-}catch{
+}catch(e){
     document.getElementById("data").innerHTML = "BAD JSON!";
     return false
 }
-ws.send(JSON.stringify({act: "fetch",key: "CatoDB",query: quey}));
+try{
+    ws.send(JSON.stringify({act: "fetch",key: "CatoDB",query: quey}));
+}
+catch{
+document.getElementById("data").style = 'color: red';
+document.getElementById("data").innerHTML = "FAILED TO CONNECT";
+document.getElementById("status").style = 'color: red';
+document.getElementById("status").innerHTML = "Offline/connection refused!";
+return false
+}
 ws.onmessage = function (msg){
     var data = JSON.parse(msg.data);
     console.log(data)
